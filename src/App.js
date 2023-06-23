7d8fa8e961a6e93aeb181fbd59583a9d40e2e654
Italo_de_Jesus <italoalvesjesus01@gmail.com>
2023-06-23 12:38:15 -0300

    atualizando o App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import InputMask from 'react-input-mask';
+import axios from 'axios';
 import './App.css';
+import Form from './Form';
+import ItemList from './ItemList';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/api/index');
+      setItems(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleFormSubmit = () => {
+    fetchData();
+  };
+
+  const handleItemUpdate = () => {
+    fetchData();
+  };
+
+  const handleItemDelete = () => {
+    fetchData();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Indique um amigo</h1>
+      <Form onFormSubmit={handleFormSubmit} />
+      <h2>Indicações realizadas</h2>
+      <ItemList
+        items={items}
+        onItemUpdate={handleItemUpdate}
+        onItemDelete={handleItemDelete}
+      />
     </div>
   );
 }
